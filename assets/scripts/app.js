5d299bcbd1cf9470a65e321f7a723f9d852adf05
kavliukigor <kavliukigor@gmail.com>
2026-06-10 10:40:52 +0300

    refactor: enhance arithmetic functions to utilize user input and improve output formatting

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,37 @@
-const defaultResult = 0
+const defaultResult = 0;
 let currentResult = defaultResult
 
+function getUserNumberInput(){
+    return parseInt(userInput.value)
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
+}
+
 function add() {
-    currentResult = currentResult + userInput.value
-    outputResult(currentResult, '')
+    const enteredNumber = getUserNumberInput()
+    currentResult = currentResult + enteredNumber
+    createAndWriteOutput('+', currentResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput()
+    currentResult = currentResult + enteredNumber
+    createAndWriteOutput('-', currentResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput()
+    currentResult = currentResult + enteredNumber
+    createAndWriteOutput('*', currentResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput()
+    currentResult = currentResult + enteredNumber
+    createAndWriteOutput('/', currentResult, enteredNumber)
 }
 
 addBtn.addEventListener('click', add)
